edc726c93f9459015f0218b1cb0d7f956a4f3adf
pmj2744 <pmj2744@gmail.com>
2026-03-23 16:26:55 +0900

    1st

---
+++ main.js
@@ -0,0 +1,24 @@
+document.getElementById('time').innerHTML = new Date().toLocaleString()
+
+function Pink() {
+  document.body.style.backgroundColor = 'Pink'
+}
+function lightblue() {
+  document.body.style.backgroundColor = 'lightblue'
+}
+function yellow() {
+  document.body.style.backgroundColor = 'yellow'
+}
+function reset() {
+  document.body.style.backgroundColor = 'reset'
+}
+
+function showjs() {
+  document.getElementById('fig').src = 'img/javascript.png'
+  document.getElementById('desc').innerHTML =
+}
+
+function hide() {
+  document.getElementById
+  document.
+}
